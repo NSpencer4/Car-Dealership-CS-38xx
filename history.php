2405bae3d2405e8c564640b8c16cb1f2e4e6fbbd
nspencer4 <nicholas.spencer@scotts.com>
2017-11-20 20:15:06 -0500

    Redirect user if not logged in

--- history.php
+++ history.php
@@ -19,13 +19,20 @@ if (!isset($_SESSION['customers'])) {
 <main id="container" class="cards">
     <?php include('templates/header.php'); ?>
     <section id="main-container">
-        <h2>Service History Page</h2>
-        <p>
+        <?php
+        if (!isset($_SESSION['login-type'])) {
+            echo "You are not logged in. Please login and try again. Redirecting to the homepage in 3 seconds.";
+            header( "refresh:3; url=index.php" );
+        } else {
+            echo "<h2>Service History Page</h2>";
+            echo "<p>";
+            echo "Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!
             Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!
             Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!
-            Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!
-            Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!
-        </p><br>
+            Hodor, HODOR hodor, hodor hodor... Hodor hodor hodor? Hodor, hodor; hodor hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor hodor - hodor... Hodor hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor!";
+            echo "</p><br>";
+        }
+        ?>
     </section>
     <?php include('templates/footer.php'); ?>
 </main>
