bbfd361c6c5925329fbb7c95662c80165b81b0de
nspencer4 <nicholas.spencer@scotts.com>
2017-11-20 20:18:03 -0500

    Short description done

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,7 @@
 echo "<header id='form-title-container'>";
 echo "<a href='index.php'><img id='logo' src='images/audir8.jpg'></a>";
 echo "<h1>BHowdy's Exotic Car Dealership</h1><br>";
+echo "<h4>Welcome to our dealership website! Here you can view our inventory, schedule a vehicle service and view your past services with us.</h4><br>";
 echo '<a href="index.php" <button class="btn modify margin-left">Showroom</button></a>';
 echo '<a href="schedule.php" <button class="btn modify margin-left">Schedule A Service</button></a>';
 echo '<a href="history.php" <button class="btn modify margin-left">Service History</button></a>';
