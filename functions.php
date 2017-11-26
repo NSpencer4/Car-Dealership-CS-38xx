0137314ca4e043296f394e2e3949a401c984f1c5
nspencer <n.chase.spencer@gmail.com>
2017-11-26 16:14:52 -0500

    queries for appointments and services

--- functions.php
+++ functions.php
@@ -8,24 +8,34 @@
  */
 class functions
 {
-    public function submit_service_req($db) {
-        $date = datetime($_POST['service_date']);
+    public function get_appointments($db) {
+      $query = "SELECT service_id, appt_time, cust_email from Appointments";
 
-        $query = "INSERT INTO scores (dateCompleted, score)
-              VALUES ('$date', $correct_count)";
-
-        $statement = $db->prepare($query);
-        $success = $statement->execute();
-        $statement->closeCursor();
+      $statement = $db->prepare($query);
+      $statement->execute();
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+      $statement->closeCursor();
+      return $result;
     }
 
-    public function getPrevAttempts($db) {
-        $query = "SELECT dateCompleted, score from scores";
+    public function get_services($db) {
+      $query = "SELECT service_id, serv_description from Services";
 
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $statement->closeCursor();
-        return $result;
+      $statement = $db->prepare($query);
+      $statement->execute();
+      $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+      $statement->closeCursor();
+      return $result;
     }
+
+    // public function getPrevAttempts($db) {
+    //   $date = datetime($_POST['service_date']);
+    //
+    //   $query = "INSERT INTO scores (dateCompleted, score)
+    //         VALUES ('$date', $correct_count)";
+    //
+    //   $statement = $db->prepare($query);
+    //   $success = $statement->execute();
+    //   $statement->closeCursor();
+    // }
 }
